1a79b72839395a3c43e3ec374c78af2d487a105d
cmvandevprojects <54376653+cmvandevprojects@users.noreply.github.com>
2020-05-17 02:43:06 -0700

    Update BDM_FinalChallenge.py

--- BDM_FinalChallenge.py
+++ BDM_FinalChallenge.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
@@ -13,10 +18,12 @@ from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import broadcast
 from pyspark.sql.functions import regexp_replace
 from pyspark.sql import functions as f
-spark = SparkSession.builder.getOrCreate()
+#spark = SparkSession.builder.getOrCreate()
+sc = SparkContext()
+spark = SparkSession(sc)
 
 
-# In[3]:
+# In[2]:
 
 
 #NEW CHANGES
@@ -26,7 +33,7 @@ spark = SparkSession.builder.getOrCreate()
 # filter years with an and statement
 
 
-# In[4]:
+# In[3]:
 
 
 # read data
@@ -38,7 +45,7 @@ centerlines = spark.read.csv('hdfs:///tmp/bdm/nyc_cscl.csv', header=True, inferS
 
 # ### Preprocessing
 
-# In[5]:
+# In[4]:
 
 
 vio_rel = violations.select(violations['House Number'], upper(violations['Street Name']).alias('Street Name'), upper(violations['Violation County']).alias('Violation County'), violations['Issue Date'])
@@ -49,13 +56,13 @@ vio_rel = vio_rel.withColumn('Year', vio_rel['Issue Date'].substr(-4,4))
 vio_rel = vio_rel.filter((vio_rel["Year"]=="2015")|(vio_rel["Year"] == "2016")|(vio_rel["Year"] == "2017")|(vio_rel["Year"] == "2018")|(vio_rel["Year"] == "2019"))
 
 
-# In[6]:
+# In[5]:
 
 
 cent_rel = centerlines.select(centerlines['PHYSICALID'], upper(centerlines['FULL_STREE']).alias('FULL_STREE'), upper(centerlines['ST_LABEL']).alias('ST_LABEL'), centerlines['BOROCODE'], centerlines['L_LOW_HN'], centerlines['L_HIGH_HN'], centerlines['R_LOW_HN'], centerlines['R_HIGH_HN'])
 
 
-# In[7]:
+# In[6]:
 
 
 # cache dataframes
@@ -63,7 +70,7 @@ vio_rel.cache()
 cent_rel.cache()
 
 
-# In[8]:
+# In[7]:
 
 
 # drop rows with nulls
@@ -71,7 +78,7 @@ vio_rel = vio_rel.na.drop()
 cent_rel = cent_rel.na.drop()
 
 
-# In[9]:
+# In[8]:
 
 
 # violation data preprocessing
@@ -80,13 +87,13 @@ vio_rel = vio_rel.withColumn("Clean House Number", regexp_replace(vio_rel["House
 vio_rel = vio_rel.na.drop()
 
 
+# In[9]:
+
+
+
+
+
 # In[10]:
-
-
-
-
-
-# In[11]:
 
 
 # centerline data preprocessing
@@ -103,13 +110,13 @@ cent_rel = cent_rel.withColumn("Clean R_HIGH_HN", regexp_replace(cent_rel["R_HIG
 cent_rel = cent_rel.na.drop()
 
 
+# In[11]:
+
+
+
+
+
 # In[12]:
-
-
-
-
-
-# In[13]:
 
 
 # county to borocode mapping for violation dataset
@@ -123,13 +130,13 @@ vio_rel = vio_rel.na.drop() #in case there were any incorrect counties recorded
 
 
 
-# In[14]:
+# In[13]:
 
 
 vio_rel.show()
 
 
-# In[15]:
+# In[14]:
 
 
 # add IsEven
@@ -137,19 +144,19 @@ vio_rel = vio_rel.withColumn("IsEven", f.when((f.col('Clean House Number')%2 == 
 vio_rel.show()
 
 
-# In[16]:
+# In[15]:
 
 
 cent_rel.dtypes
 
 
-# In[17]:
+# In[16]:
 
 
 vio_rel.dtypes
 
 
-# In[18]:
+# In[17]:
 
 
 # drop unused columns before the join
@@ -158,13 +165,13 @@ vio_rel = vio_rel.drop("House Number","Issue Date","Violation County")
 cent_rel = cent_rel.drop("L_LOW_HN","L_HIGH_HN","R_LOW_HN","R_HIGH_HN")
 
 
-# In[19]:
+# In[18]:
 
 
 vio_rel.show()
 
 
-# In[20]:
+# In[19]:
 
 
 cent_rel.show()
@@ -172,57 +179,57 @@ cent_rel.show()
 
 # ### Union
 
-# In[30]:
+# In[20]:
 
 
 c1 = cent_rel.drop("FULL_STREE")
 c2 = cent_rel.drop("ST_LABEL")
 
 
-# In[ ]:
+# In[21]:
 
 
 (violations['Street Name']).alias('Street Name')
 
 
-# In[31]:
+# In[22]:
 
 
 c1.show()
 
 
-# In[48]:
+# In[23]:
 
 
 vio_rel.show()
 
 
-# In[32]:
+# In[24]:
 
 
 c2.show()
 
 
-# In[39]:
+# In[25]:
 
 
 c3 = c1.union(c2).distinct().sort("PHYSICALID")
 c3.show()
 
 
-# In[40]:
+# In[26]:
 
 
 
 
 
-# In[37]:
+# In[27]:
 
 
 
 
 
-# In[38]:
+# In[28]:
 
 
 
@@ -230,14 +237,14 @@ c3.show()
 
 # ## Join
 
-# In[41]:
+# In[29]:
 
 
 from pyspark.sql.functions import broadcast
 #df = vio_clean.join(broadcast(cent_clean), (((vio_clean["Street Name"]==cent_clean["FULL_STREE"])|(vio_clean["Street Name"]==cent_clean["FULL_STREE"]))&(vio_clean["MappedBorocode"]==cent_clean["Borocode"])&((vio_clean["IsEven"]=="yes")&(vio_clean["Clean House Number"]<=cent_clean["Clean R_HIGH_HN"])&(vio_clean["Clean House Number"]>=cent_clean["Clean R_Low_HN"]))))
 
 
-# In[42]:
+# In[30]:
 
 
 df = vio_rel.join(broadcast(c3), 
@@ -249,20 +256,32 @@ df = vio_rel.join(broadcast(c3),
                    )
 
 
-# In[43]:
+# In[31]:
 
 
 # cache dataframe
 df.cache()
 
 
-# In[44]:
+# In[32]:
 
 
 df.dtypes
 
 
-# In[45]:
+# In[33]:
+
+
+
+
+
+# In[34]:
+
+
+
+
+
+# In[35]:
 
 
 
@@ -270,133 +289,47 @@ df.dtypes
 
 # ## Post Processing
 
-# In[151]:
+# In[36]:
 
-vio_rel.unpersist()
-cent_rel.unpersist()
+
 output = df.select(df['PHYSICALID'], df['Year']).groupBy("PHYSICALID").pivot("Year",["2015","2016","2017","2018","2019"]).count().sort("PHYSICALID").na.fill(0)
-output.write.csv('final4')
 
 
-# In[153]:
+# In[37]:
 
 
 
 
-# In[154]:
+
+# In[38]:
 
 
 output.show(5)
 
 
-# In[399]:
+# ## OLS
+
+# In[42]:
 
 
-# add physical IDs with no violation back
+import statsmodels.api as sm
 
 
-# In[ ]:
+# In[50]:
 
 
+output = output.withColumn('slope', sm.OLS([output['2015'],output['2016'],output['2017'],output['2018'],output['2019']],[1,2,3,4,5]).fit().params[0])
 
 
-
-# In[ ]:
-
+# In[51]:
 
 
+output.show()
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+output.write.csv('cmv1')
 
 
 # In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-# TO DO: groupBy house number/street and count before the join
-# Fix the compound house issues
-# add
 
 
 
